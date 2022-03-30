7bf168c61b0f53789665424e412cfbd6a3293c4a
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2022-03-29 20:32:32 -0700

    INFRA: Initialize Build Project

---
+++ Ezregx.Infrastructure.Build/Program.cs
@@ -0,0 +1,71 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// ---------------------------------------------------------------
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var adotnetClient = new ADotNetClient();
+
+var githubPipeline = new GithubPipeline
+{
+    Name = ".Net",
+
+    OnEvents = new Events
+    {
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2019,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Check Out"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Setup Dot Net Version",
+
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "6.0.3"
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restore"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Build"
+                },
+
+                new TestTask
+                {
+                    Name = "Test"
+                }
+            }
+        }
+    }
+};
+
+adotnetClient.SerializeAndWriteToFile(githubPipeline, "../../../../.github/workflows/dotnet.yml");
