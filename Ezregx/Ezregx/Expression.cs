ae89af193f68439ad10fe8385205dbe0e85cf688
Hassan Habib <hassanhabib@live.com>
2022-03-29 19:51:06 -0700

    INFRA: Initialize Library Project

---
+++ Ezregx/Ezregx/Expression.cs
@@ -0,0 +1,11 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// -------------------------------------------------------
+
+namespace Ezregx
+{
+    public class Expression
+    {
+
+    }
+}
